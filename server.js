046e30d5e607902700f78b754a65cbbfea798d79
Sibananda Nayak <nayaksibananda22@gmail.com>
2023-06-22 20:18:41 +0530

    Initial commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 4040;
 
 
 const path = require('path')
+
 app.get('/',(req,res)=>{
     app.use(express.static(path.resolve(__dirname,'client','build')))
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
